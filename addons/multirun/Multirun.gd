149a14c11480c72d7f28a6317bc5ae734d7bc88c
DavidPgl <panagiotopulos.d@gmail.com>
2021-09-27 18:17:33 +0200

    Add button for closing main window and all 'Multirun windows'

--- addons/multirun/Multirun.gd
+++ addons/multirun/Multirun.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 var panel1
 var panel2
+var panel3
 var pids = []
 
 func _enter_tree():
@@ -11,19 +12,23 @@ func _enter_tree():
 	var icon_transition = gui_base.get_icon("TransitionSync", "EditorIcons") #ToolConnect
 	var icon_transition_auto = gui_base.get_icon("TransitionSyncAuto", "EditorIcons")
 	var icon_load = gui_base.get_icon("Load", "EditorIcons")
+	var icon_stop = gui_base.get_icon("Stop", "EditorIcons")
 	
 	panel2 = _add_tooblar_button("_loaddir_pressed", icon_load, icon_load)
 	panel1 = _add_tooblar_button("_multirun_pressed", icon_transition, icon_transition_auto)
+	panel3 = _add_tooblar_button("_stop_pressed", icon_stop, icon_stop)
 	
 	_add_setting("debug/multirun/number_of_windows", TYPE_INT, 2)
-	_add_setting("debug/multirun/window_distance", TYPE_INT, 1270)
+	_add_setting("debug/multirun/window_distance_x", TYPE_INT, 256)
+	_add_setting("debug/multirun/window_distance_y", TYPE_INT, 64)
 	_add_setting("debug/multirun/add_custom_args", TYPE_BOOL, false)
 	_add_setting("debug/multirun/first_window_args", TYPE_STRING, "")
 	_add_setting("debug/multirun/other_window_args", TYPE_STRING, "")
 
 func _multirun_pressed():
 	var window_count : int = ProjectSettings.get_setting("debug/multirun/number_of_windows")
-	var window_dist : int = ProjectSettings.get_setting("debug/multirun/window_distance")
+	var window_dist_x : int = ProjectSettings.get_setting("debug/multirun/window_distance_x")
+	var window_dist_y : int = ProjectSettings.get_setting("debug/multirun/window_distance_y")
 	var add_custom_args : bool = ProjectSettings.get_setting("debug/multirun/add_custom_args")
 	var first_args : String = ProjectSettings.get_setting("debug/multirun/first_window_args")
 	var other_args : String = ProjectSettings.get_setting("debug/multirun/other_window_args")
@@ -42,7 +47,7 @@ func _multirun_pressed():
 
 	kill_pids()
 	for i in range(window_count-1):
-		commands = ["--position", str(50 + (i+1) * window_dist) + ",10"]
+		commands = ["--position", str(50 + (i+1) * window_dist_x) + "," + str(10 + (i+1) * window_dist_y)]
 		if other_args && add_custom_args:
 			for arg in other_args.split(" "):
 				commands.push_front(arg)
@@ -50,6 +55,11 @@ func _multirun_pressed():
 
 func _loaddir_pressed():
 	OS.shell_open(OS.get_user_data_dir())
+	
+func _stop_pressed():
+	kill_pids()
+	var interface = get_editor_interface()
+	interface.stop_playing_scene()
 
 func _exit_tree():
 	_remove_panels()
@@ -67,6 +77,9 @@ func _remove_panels():
 	if panel2:
 		remove_control_from_container(CONTAINER_TOOLBAR, panel2)
 		panel2.free()
+	if panel3:
+		remove_control_from_container(CONTAINER_TOOLBAR, panel3)
+		panel3.free()
 
 func _unhandled_input(event):	
 	if event is InputEventKey:
