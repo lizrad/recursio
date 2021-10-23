2f18742b195ee3540b5076e4bbc0bce4e3c4721a
karl <karl.bittner@hexaquo.at>
2021-10-23 18:17:59 +0200

    Vary ghost path color
    this makes them easier to tell apart. closes #57

--- recursio-client/Players/GhostPath.gd
+++ recursio-client/Players/GhostPath.gd
@@ -1,5 +1,16 @@
 extends Spatial
 
 
+var index_colors = {
+	0: Color(0.0, 1.0, 0.0),
+	1: Color(1.0, 1.0, 0.0),
+	2: Color(0.0, 1.0, 1.0)
+}
+
+
 func set_curve(curve):
 	$Path.curve = curve
+
+
+func set_color_for_index(index):
+	$Path/CSGPolygon.material_override.albedo_color = index_colors[index]
