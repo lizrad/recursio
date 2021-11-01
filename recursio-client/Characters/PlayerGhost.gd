4577bd51526d2fd13395af0704a01f9aaa5a51cd
karl <karl.bittner@hexaquo.at>
2021-11-01 22:11:54 +0100

    Fix errors in ghost path visualization
    Still not visible sometimes, but that seems to be related to sync issues

--- recursio-client/Characters/PlayerGhost.gd
+++ recursio-client/Characters/PlayerGhost.gd
@@ -1,24 +1,21 @@
 extends Ghost
 class_name PlayerGhost
 
-onready var _path_scene = preload("res://Rendering/GhostPath.tscn").instance()
-var _path: Curve3D
-
 
 func player_ghost_init(action_manager, record_data: RecordData, color_scheme: String) -> void:
 	.ghost_init(action_manager, record_data, color_scheme)
 
 
 func create_path():
-		_path = Curve3D.new()
+		var _curve = Curve3D.new()
 		
 		for frame in range(0, _record_data.record_frames.size(), 30):
 			var record_frame: RecordFrame = _record_data.record_frames[frame]
-			_path.add_point(record_frame.position)
+			_curve.add_point(record_frame.position)
 		
-		_path_scene.set_curve(_path)
-		_path_scene.set_color_for_index(timeline_index)
-		add_child(_path_scene)
+		$GhostPath.set_curve(_curve)
+		$GhostPath.set_color_for_index(timeline_index)
+
 
 func delete_path():
-	_path_scene.queue_free()
+	$GhostPath.set_curve(Curve.new())
