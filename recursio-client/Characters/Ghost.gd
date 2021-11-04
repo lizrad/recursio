6ebb5e459ff3d48cc4e638ffd32a1f4578683f57
incredibleLeitman <michael.leithner@gmail.com>
2021-11-04 10:50:32 +0100

    * disable player model on death instead of moving out of bounds

--- recursio-client/Characters/Ghost.gd
+++ recursio-client/Characters/Ghost.gd
@@ -19,6 +19,7 @@ func ghost_init(action_manager, record_data: RecordData) -> void:
 	.ghost_base_init(action_manager, record_data)
 	_character_model._set_color_scheme("enemy_ghost", record_data.timeline_index)
 	
+	print("ghost init")
 	var wall_index = Constants.get_value("ghosts","wall_placing_timeline_index")
 	# TODO: find a better way to detect enemy... maybe with groups or owning team or smth?
 	var friendly = "PlayerGhost" in name
@@ -26,24 +27,26 @@ func ghost_init(action_manager, record_data: RecordData) -> void:
 		_minimap_alive = _minimap_icon_alive_hitscan if wall_index != timeline_index else _minimap_icon_alive_wall
 	else:
 		_minimap_alive = _minimap_icon_alive_hitscan_enemy if wall_index != timeline_index else _minimap_icon_alive_wall_enemy
-	
 
 # OVERRIDE #
-func start_playing(start_time: int) -> void: 
+func start_playing(start_time: int) -> void:
 	.start_playing(start_time)
-	_minimap_icon.set_texture(_minimap_alive)
 
 # Displays ghost as dead and triggers the base classes hit function
 # OVERRIDE #
-func hit():
+func hit() -> void:
 	.hit()
+	# TODO: apply death animation and mesh
 	_minimap_icon.set_texture(_minimap_icon_dead)
-	#_mesh.rotate_z(90)
+	_character_model.visible = false
 
-func enable_body():
+func enable_body() -> void:
 	if not _kinematic_body.is_inside_tree():
 		add_child(_kinematic_body)
+	
+	_minimap_icon.set_texture(_minimap_alive)
+	_character_model.visible = true
 
-func disable_body():
+func disable_body() -> void:
 	if _kinematic_body.is_inside_tree():
 		remove_child(_kinematic_body)
