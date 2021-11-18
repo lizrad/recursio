48ad076d47826ed7839d13a8f2f4cc9e74a80e51
Karl <karl.bittner@hexaquo.at>
2021-11-18 11:22:02 +0100

    Finish second phase of tutorial

--- recursio-client/Tutorial/Tutorial.gd
+++ recursio-client/Tutorial/Tutorial.gd
@@ -1,6 +1,15 @@
 extends Spatial
 
 
+enum Phases {
+	ROUND1,
+	ROUND2,
+	DONE
+}
+
+var current_phase = Phases.ROUND1
+
+
 func _ready():
 #	$Player.player_init($CharacterManager/ActionManager, $CharacterManager/RoundManager)
 #	$Player.block_movement = false
@@ -28,6 +37,16 @@ func _ready():
 
 
 func _process(delta):
+	if current_phase == Phases.ROUND1:
+		_update_phase1()
+	elif current_phase == Phases.ROUND2:
+		_update_phase2()
+	elif current_phase == Phases.DONE:
+		# TODO: Quit tutorial
+		pass
+
+
+func _update_phase1():
 	if $LevelH.get_capture_points()[1]._capture_progress >= 0.9 \
 			and $LevelH.get_capture_points()[1]._capture_progress < 1.0:
 		$LevelH.get_capture_points()[1]._capture_progress = 1.0
@@ -39,5 +58,17 @@ func _process(delta):
 		$CharacterManager/RoundManager.round_index += 1
 		$CharacterManager/RoundManager.switch_to_phase(RoundManager.Phases.PREPARATION)
 		$CharacterManager._on_player_ghost_record_received(0, $CharacterManager.get_player().get_record_data())
+		current_phase = Phases.ROUND2
 		
-		$TutorialUI/PanelContainer/TutorialText.text = "Now try capturing the other point too."
+		$TutorialUI/PanelContainer/TutorialText.text = "Now try capturing both points."
+		yield(get_tree().create_timer(3.0), "timeout")
+		$TutorialUI/PanelContainer/TutorialText.text = "Your past self will help you!"
+
+
+func _update_phase2():
+	if $LevelH.get_capture_points()[1]._capture_progress >= 0.9 \
+			and $LevelH.get_capture_points()[1]._capture_progress < 1.0 \
+			and $LevelH.get_capture_points()[0]._capture_progress >= 0.9 \
+			and $LevelH.get_capture_points()[0]._capture_progress < 1.0:
+		$TutorialUI/PanelContainer/TutorialText.visible = true
+		$TutorialUI/PanelContainer/TutorialText.text = "Good job!"
