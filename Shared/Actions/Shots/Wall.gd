8d2f8606c1771742b65f7cf95053944475e9c144
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-11-24 17:26:30 +0100

    Add wall VFX

--- Shared/Actions/Shots/Wall.gd
+++ Shared/Actions/Shots/Wall.gd
@@ -1,12 +1,29 @@
 extends StaticBody
 
+export var animation_time:= 3.0
 
+onready var _mesh_pivot = get_node("MeshPivot")
+onready var _mesh_instance = get_node("MeshPivot/MeshInstance")
+var _time_since_spawn := 0.0
 var placed_by_body
 var round_index
 
 func _init() -> void:
 	Logger.info("_init action", "Wall")
 
+#adapted from: https://easings.net/#easeOutElastic
+func ease_out_elastic(x: float, wobble: float) -> float:
+	var c4 = (2.0 * PI) / 3.0
+	return 0.0 if x == 0 else (1.0 if x == 1 else  pow(2.0, -10.0 * x) * sin((x * wobble - 0.75) * c4) + 1.0);
+
+func _process(delta):
+	_time_since_spawn += delta
+	var ratio = min(1, _time_since_spawn/animation_time)
+	var remapped_ratio = ease_out_elastic(ratio,15.0)
+	_mesh_pivot.scale = Vector3(1, remapped_ratio, 1)
+	
+	if _time_since_spawn >= animation_time:
+		set_physics_process(false)
 
 func _ready():
 	var _error = $KillGhostArea.connect("body_entered", self, "handle_hit") 
@@ -25,8 +42,8 @@ func initialize_visual(owning_player) -> void:
 		var character_model_controller = owning_player.get_node("KinematicBody/CharacterModel")
 		var color_scheme = character_model_controller.color_scheme
 		color = Color(Constants.get_value("colors", color_scheme + "_main"))
-	$MeshInstance.material_override.albedo_color = color
-	$MeshInstance.material_override.emission = color
+	_mesh_instance.material_override.albedo_color = color
+	_mesh_instance.material_override.emission = color
 
 
 func handle_hit(collider):
--- recursio-client/Characters/Visuals/Animation/WallAnimator.gd
+++ recursio-client/Characters/Visuals/Animation/WallAnimator.gd
@@ -1,12 +1,25 @@
 extends BaseAnimator
 
+onready var _wall_particles = get_node("../RootPivot/MiddlePivot/WallParticles")
+
 export var animation_duration := 0.5
 
 
 var _time_since_start = 0
+var _particles_played = false
+
+func _ready():
+	var _error =  get_parent().connect("color_scheme_changed",self,"_on_color_scheme_changed")
+	_wall_particles.lifetime = animation_duration;
+
+func _on_color_scheme_changed(new_color_scheme, _timeline_index):
+	var accent_color = Color(Constants.get_value("colors",new_color_scheme + "_secondary_accent"))
+	_wall_particles.material_override.albedo_color = accent_color
+	_wall_particles.material_override.emission = accent_color
 
 func start_animation():
 	_time_since_start = 0
+	_particles_played = false
 
 func get_keyframe(delta):
 	_reset_keyframes()
@@ -15,6 +28,9 @@ func get_keyframe(delta):
 		emit_signal("animation_over")
 		_time_since_start = animation_duration
 	var ratio = _time_since_start/animation_duration
+	if ratio >= 0.5 and not _particles_played:
+		_wall_particles.restart()
+		_particles_played = true
 	
 	var remapped_ratio = abs(1-2*pow(1-ratio,2))
 	var scale = Vector3(remapped_ratio,remapped_ratio,remapped_ratio)
