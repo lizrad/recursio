cb54bbe37f29816d2af61235f08837e0b4a44b9d
Karl <karl.bittner@hexaquo.at>
2021-11-30 14:23:21 +0100

    Add possibility to run tutorial without texts
    good for presentations

--- recursio-client/Tutorial/Tutorial.gd
+++ recursio-client/Tutorial/Tutorial.gd
@@ -8,27 +8,37 @@ enum Phases {
 }
 
 var current_phase = Phases.ROUND1
+var show_text = true
+
+
+func set_ui_visible(is_visible):
+	$TutorialUI.visible = is_visible if show_text else false
 
 
 func _ready():
+	set_ui_visible(true)
+	
 #	$Player.player_init($CharacterManager/ActionManager, $CharacterManager/RoundManager)
 #	$Player.block_movement = false
 
 	$TutorialUI/PanelContainer/TutorialText.text = "Welcome to the tutorial!"
-	yield(get_tree().create_timer(3.0), "timeout")
+	if show_text:
+		yield(get_tree().create_timer(3), "timeout")
 	
 	$CharacterManager._on_spawn_player(0, Vector3.ZERO, 0)
 	$CharacterManager.get_player().kb.visible = false
 	
 	$TutorialUI/PanelContainer/TutorialText.text = "The goal is to capture both points at once."
-	yield(get_tree().create_timer(4.0), "timeout")
+	if show_text:
+		yield(get_tree().create_timer(4), "timeout")
 	
 	$CharacterManager.get_player().set_custom_view_target($LevelH.get_capture_points()[1])
 	$TutorialUI/PanelContainer/TutorialText.text = "Try capturing this one!"
-	yield(get_tree().create_timer(3.0), "timeout")
+	if show_text:
+		yield(get_tree().create_timer(3), "timeout")
 	
 	$CharacterManager.get_player().follow_camera()
-	$TutorialUI.visible = false
+	set_ui_visible(false)
 	
 	$CharacterManager.get_player().kb.visible = true
 	
@@ -51,10 +61,11 @@ func _update_phase1():
 	if $LevelH.get_capture_points()[1]._capture_progress >= 0.9 \
 			and $LevelH.get_capture_points()[1]._capture_progress < 1.0:
 		$LevelH.get_capture_points()[1]._capture_progress = 1.0
-		$TutorialUI.visible = true
+		set_ui_visible(true)
 		$TutorialUI/PanelContainer/TutorialText.text = "Nice!"
 		
-		yield(get_tree().create_timer(3.0), "timeout")
+		if show_text:
+			yield(get_tree().create_timer(3), "timeout")
 		
 		$CharacterManager/RoundManager.round_index += 1
 		$CharacterManager/RoundManager.switch_to_phase(RoundManager.Phases.PREPARATION)
@@ -62,7 +73,8 @@ func _update_phase1():
 		current_phase = Phases.ROUND2
 		
 		$TutorialUI/PanelContainer/TutorialText.text = "Now try capturing both points."
-		yield(get_tree().create_timer(3.0), "timeout")
+		if show_text:
+			yield(get_tree().create_timer(3), "timeout")
 		$TutorialUI/PanelContainer/TutorialText.text = "Your past self will help you!"
 
 
@@ -74,7 +86,10 @@ func _update_phase2():
 		$LevelH.get_capture_points()[0]._capture_progress = 1.0
 		$LevelH.get_capture_points()[1]._capture_progress = 1.0
 		
-		$TutorialUI.visible = true
+		set_ui_visible(true)
 		$TutorialUI/PanelContainer/TutorialText.text = "Good job!"
 		
 		current_phase = Phases.DONE
+		
+		yield(get_tree().create_timer(2.0), "timeout")
+		get_tree().change_scene("res://UI/Menus/StartMenu.tscn")
