565de831a477a9613f9c54fdc4dd7751cb7f89de
karl <karl.bittner@hexaquo.at>
2021-12-03 17:56:50 +0100

    Make user settings persistent

--- recursio-client/UI/Menus/SettingsContainer.gd
+++ recursio-client/UI/Menus/SettingsContainer.gd
@@ -1,9 +1,36 @@
 extends PanelContainer
 
+export var always_apply_defaults_at_startup := false
+
+var settings_config = ConfigFile.new()
+
+const SETTINGS_PATH = "user://settings.ini"
+
 
 func _ready():
+	# Load previous settings
+	var err = settings_config.load(SETTINGS_PATH)
+
+	# If the file didn't load (first ever game start), save the defaults there initially
+	if err != OK or always_apply_defaults_at_startup:
+		settings_config.load("res://default-settings.ini") # this should never fail
+		settings_config.save(SETTINGS_PATH)
+	
 	$SettingsList/BackButton.connect("pressed", self, "_on_back_pressed")
+	
+	# Allow nodes which set a setting to access the config via this node
+	for node in get_tree().get_nodes_in_group("Setting"):
+		node.set("settings", self)
 
 
 func _on_back_pressed():
 	hide()
+
+
+func get_setting(setting_header, setting_name, default = null):
+	return settings_config.get_value(setting_header, setting_name, default)
+
+
+func set_setting(setting_header, setting_name, value):
+	settings_config.set_value(setting_header, setting_name, value)
+	settings_config.save(SETTINGS_PATH)
--- recursio-client/UI/Menus/VolumeSlider.gd
+++ recursio-client/UI/Menus/VolumeSlider.gd
@@ -5,12 +5,18 @@ export var audio_bus_name := "Master"
 
 onready var _bus := AudioServer.get_bus_index(audio_bus_name)
 
+var settings setget set_settings
+
 
 func _ready() -> void:
-	value = db2linear(AudioServer.get_bus_volume_db(_bus))
 	connect("value_changed", self, "_on_value_changed")
 
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(_bus, linear2db(value))
-	# TODO: Save this value!
+	settings.set_setting("audio", audio_bus_name.to_lower() + "_volume", value)
+
+
+func set_settings(new_settings):
+	settings = new_settings
+	value = settings.get_setting("audio", audio_bus_name.to_lower() + "_volume")
