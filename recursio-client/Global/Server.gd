b7b702f7a4c41bfc622de9deb7907346337748ed
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-14 10:19:56 +0100

    Rename timeline_pick signal to be in passive form

--- recursio-client/Global/Server.gd
+++ recursio-client/Global/Server.gd
@@ -30,7 +30,7 @@ signal game_result(winning_player_id)
 signal player_hit(hit_player_id, perpetrator_player_id, perpetrator_timeline_index)
 signal ghost_hit(hit_ghost_player_owner, hit_ghost_id, perpetrator_player_id, perpetrator_timeline_index)
 signal quiet_ghost_hit(hit_ghost_player_owner, hit_ghost_id, perpetrator_player_id, perpetrator_timeline_index)
-signal timeline_pick(picking_player_id, timeline_index)
+signal timeline_picked(picking_player_id, timeline_index)
 signal wall_spawn (position, rotation, wall_index)
 
 signal phase_switch_received(round_index,next_phase, switch_time)
@@ -267,7 +267,7 @@ remote func receive_player_action(action_player_id, action_type):
 
 remote func receive_timeline_pick(picking_player_id, timeline_index):
 	Logger.debug("Ghost pick received", "server")
-	emit_signal("timeline_pick",picking_player_id, timeline_index)
+	emit_signal("timeline_picked",picking_player_id, timeline_index)
 
 
 remote func receive_wall_spawn(position, rotation, wall_index):
