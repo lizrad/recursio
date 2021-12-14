3819f5046bbfb8e7e71f929b24bff0bbb7a3245e
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-14 22:33:46 +0100

    Store changed colors in user settings

--- recursio-client/UI/Menus/ColorSetting.gd
+++ recursio-client/UI/Menus/ColorSetting.gd
@@ -4,10 +4,24 @@ extends HBoxContainer
 onready var _color_picker_button : ColorPickerButton = get_node("ColorPickerButton")
 onready var _label :Label = get_node("Label")
 
+var _header
+var _key
+
+
+func _ready():
+	_color_picker_button.connect("color_changed", self, "_on_color_changed")
+
 
 func init(header: String, key: String):
+	_header = header
+	_key = key
 	var color_name = key.replace("_"," ").capitalize()
 	name = color_name
 	_label.text = color_name
 	var color = Color(UserSettings.get_setting(header, key))
 	_color_picker_button.color = color
+
+func _on_color_changed(color: Color):
+	var color_string = "#" + color.to_html(false)
+	print(color_string)
+	UserSettings.set_setting(_header, _key, color_string)
