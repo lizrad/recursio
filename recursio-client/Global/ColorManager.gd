91bbf48ecf3ad84afa45d26cf4cf66bf9fad6375
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-14 23:22:51 +0100

    Add coupling classes

--- recursio-client/Global/ColorManager.gd
+++ recursio-client/Global/ColorManager.gd
@@ -1,7 +1,38 @@
 extends Node
 
-var _header = "colors"
+class ObjectPropertyCoupling:
+	var object: Object
+	var property: String
+	
+	
+	func _init(object: Object, property: String):
+		self.object = object
+		self.property = property
+	
+	
+	func apply_color(color: Color):
+		object.set(property, color)
 
+
+class ObjectMethodCoupling:
+	var object: Object
+	var method: String
+	var args: Array
+
+
+	func _init(object: Object, method: String, args: Array):
+		self.object = object
+		self.method = method
+		self.args = args
+
+
+	func apply_color(color: Color):
+		var all_args = args 
+		all_args += [color]
+		object.callv(method, all_args)
+
+
+var _header: String = "colors"
 
 func _get_color(color_name: String) -> Color:
 	return Color(UserSettings.get_setting(_header, color_name))
@@ -9,12 +40,15 @@ func _get_color(color_name: String) -> Color:
 
 func color_object_by_property(color_name: String, object: Object, property: String):
 	assert(is_instance_valid(object))
+	var coupling :ObjectPropertyCoupling = ObjectPropertyCoupling.new(object, property)
 	var color = _get_color(color_name)
-	object.set(property, color)
+	coupling.apply_color(color)
+	
 
 
 # this expect the color to be the last argument in the called method
 func color_object_by_method(color_name: String, object: Object, method: String, additional_method_args: Array):
+	assert(is_instance_valid(object))
+	var coupling :ObjectMethodCoupling = ObjectMethodCoupling.new(object, method, additional_method_args)
 	var color = _get_color(color_name)
-	additional_method_args.append(color)
-	object.callv(method, additional_method_args)
+	coupling.apply_color(color)
