9407325e63671eb6ecfa58784857d24e6364072e
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-15 23:26:06 +0100

    Add gamepad controls to new settings menu

--- recursio-client/UI/Menus/ColorSetting.gd
+++ recursio-client/UI/Menus/ColorSetting.gd
@@ -1,4 +1,5 @@
 extends HBoxContainer
+class_name ColorSetting
 
 
 onready var _color_picker_button : ColorPickerButton = get_node("ColorPickerButton")
@@ -8,23 +9,38 @@ var _header
 var _key
 
 
-func _ready():
+func _ready() -> void:
 	# listening to popup_closed instead of color_changed so it really only saves 
 	# to file when the user decided
 	var _error = _color_picker_button.connect("popup_closed", self, "_on_popup_closed")
 
 
-func init(header: String, key: String):
+func init(header: String, key: String) -> void:
 	_header = header
 	_key = key
 	var color_name = key.replace("_"," ").capitalize()
-	name = color_name
+	name = _key
 	_label.text = color_name
 	var color = Color(UserSettings.get_setting(header, key))
 	_color_picker_button.color = color
 
-func _on_popup_closed():
+
+func _on_popup_closed() -> void:
 	var color = _color_picker_button.color
 	var color_string = "#" + color.to_html(false)
 	UserSettings.set_setting(_header, _key, color_string)
 	ColorManager.color_changed(_key)
+
+
+func set_next_focus(focus: NodePath) -> void:
+	$ColorPickerButton.focus_next = focus
+	$ColorPickerButton.focus_neighbour_bottom = focus
+
+
+func set_previous_focus(focus: NodePath) -> void:
+	$ColorPickerButton.focus_previous = focus
+	$ColorPickerButton.focus_neighbour_top = focus
+
+
+func get_color_picker() -> Node:
+	return $ColorPickerButton
--- recursio-client/UI/Menus/ColorSettingController.gd
+++ recursio-client/UI/Menus/ColorSettingController.gd
@@ -3,12 +3,38 @@ extends ScrollContainer
 var _color_setting_scene = preload("res://UI/Menus/ColorSetting.tscn")
 
 onready var _color_list = get_node("ColorList")
+onready var _settings_tabs = get_node("..")
+
+var color_settings :Array = []
 
 
-func _ready():
+func _ready() -> void:
 	var header = ColorManager.header
 	var colors = UserSettings.get_all_settings_for_header(header)
 	for color in colors:
 		var color_setting = _color_setting_scene.instance()
 		_color_list.add_child(color_setting)
 		color_setting.init(header, color)
+		color_settings.append(color_setting)
+	
+	for i in range(1, color_settings.size()):
+		var prev_color_setting : ColorSetting = color_settings[i-1]
+		var cur_color_setting : ColorSetting = color_settings[i]
+		prev_color_setting.set_next_focus(NodePath("../../"+cur_color_setting.name+"/ColorPickerButton"))
+		cur_color_setting.set_previous_focus(NodePath("../../"+prev_color_setting.name+"/ColorPickerButton"))
+	
+	color_settings.front().set_previous_focus(NodePath("../../../../../BackButton"))
+	color_settings.back().set_next_focus(NodePath("../../../../../BackButton"))
+
+
+func get_next_focus_for_back_button() -> NodePath:
+	return NodePath("../SettingsTabs/Colors/ColorList/"+color_settings.front().name+"/ColorPickerButton")
+
+
+func get_previous_focus_for_back_button() -> NodePath:
+	return NodePath("../SettingsTabs/Colors/ColorList/"+color_settings.back().name+"/ColorPickerButton")
+
+
+
+func get_default_tab_focus() -> Node:
+	return color_settings.front().get_color_picker()
