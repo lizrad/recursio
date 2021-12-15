20c0842959e6acb0692fe8a88114889e2fb2aa9d
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-15 21:29:27 +0100

    Use default settings if no user setting is available

--- recursio-client/Global/UserSettings.gd
+++ recursio-client/Global/UserSettings.gd
@@ -5,17 +5,20 @@ signal setting_changed(setting_header, setting_name)
 export var always_apply_defaults_at_startup := false
 
 var settings_config = ConfigFile.new()
+var default_settings_config = ConfigFile.new()
 
 const SETTINGS_PATH = "user://settings.ini"
+const DEFAULT_SETTINGS_PATH = "res://default-settings.ini"
 
 
 func _ready():
 	# Load previous settings
 	var err = settings_config.load(SETTINGS_PATH)
+	var _err = default_settings_config.load(DEFAULT_SETTINGS_PATH) # this should never fail
 
 	# If the file didn't load (first ever game start), save the defaults there initially
 	if err != OK or always_apply_defaults_at_startup:
-		settings_config.load("res://default-settings.ini") # this should never fail
+		settings_config = default_settings_config
 		settings_config.save(SETTINGS_PATH)
 
 
@@ -26,7 +29,11 @@ func _input(event: InputEvent) -> void:
 
 
 func get_setting(setting_header, setting_name, default = null):
-	return settings_config.get_value(setting_header, setting_name, default)
+	return settings_config.get_value(setting_header, setting_name, get_default_setting(setting_header, setting_name, default))
+
+
+func get_default_setting(setting_header, setting_name, default = null):
+	return default_settings_config.get_value(setting_header, setting_name, default)
 
 
 func set_setting(setting_header, setting_name, value):
@@ -36,4 +43,4 @@ func set_setting(setting_header, setting_name, value):
 
 
 func get_all_settings_for_header(setting_header):
-	return settings_config.get_section_keys(setting_header)
+	return default_settings_config.get_section_keys(setting_header)
