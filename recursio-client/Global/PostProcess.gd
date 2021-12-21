f54c201825d553000a5c1e62b0670745d6670afe
karl <karl.bittner@hexaquo.at>
2021-12-21 16:51:02 +0100

    Make PostProcess.animate_property more generic

--- recursio-client/Global/PostProcess.gd
+++ recursio-client/Global/PostProcess.gd
@@ -3,7 +3,6 @@ extends Node
 var _post_process_tool_scene = preload("res://addons/PostProcessingTool/PostProcessing_tool.tscn")
 
 var _pp
-var _tween
 
 #glitch
 var glitch :bool = false setget set_glitch, get_glitch
@@ -13,20 +12,17 @@ var vignette_softness: float = 1.0 setget set_vignette_softness, get_vignette_so
 
 
 func _ready():
-	_tween = Tween.new()
-	add_child(_tween)
 	_pp = _post_process_tool_scene.instance()
 	get_parent().call_deferred("add_child", _pp)
 
 
-func remove_animation(property: NodePath):
-	_tween.remove(self, property)
-
-
 func animate_property(property: NodePath, initial_val, final_val, duration: float, trans_type = Tween.TRANS_LINEAR, ease_type = Tween.EASE_OUT):
-	remove_animation(property)
-	_tween.interpolate_property(self, property, initial_val, final_val, duration, trans_type, ease_type)
-	_tween.start()
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(self, property, initial_val, final_val, duration, trans_type, ease_type)
+	tween.start()
+	tween.connect("tween_all_completed", tween, "queue_free")
+
 
 func set_glitch(value):
 	_pp.glitch_show = value
