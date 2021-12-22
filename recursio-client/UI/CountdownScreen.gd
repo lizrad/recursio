5f0a3f830605b2656924ce7676e7eea3578c3fed
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-22 16:10:23 +0100

    Fix countdown numbers being blurry

--- recursio-client/UI/CountdownScreen.gd
+++ recursio-client/UI/CountdownScreen.gd
@@ -1,16 +1,14 @@
 extends Control
 
 
-export var scale_mod := 8
+export var scale_mod := 300
 export var scale_time := 0.5
 
-
-onready var start_pos = $CountdownText.get_transform().get_origin()
-
-
 var _text := ""
-var _countdown_time: float = Constants.get_value("gameplay", "countdown_phase_seconds")
+var _countdown_time: float = 0
 
+func _ready() -> void:
+	set_physics_process(false)
 
 func _process(delta) -> void:
 	_update_text(int(_countdown_time))
@@ -23,25 +21,23 @@ func _process(delta) -> void:
 func activate() -> void:
 	show()
 	_countdown_time = Constants.get_value("gameplay","countdown_phase_seconds")
+	set_physics_process(true)
 
 
 func deactivate() -> void:
 	hide()
 	_countdown_time = 0
+	set_physics_process(false)
 
 
 func _update_text(sec: float) -> void:
 	var text = str(sec) if sec > 0 else "GO!"
-	
 	if _text != text:
 		_text = text
-		
 		if sec > 0:
 			$ProgressSound.play()
 		else:
 			$GoSound.play()
-
 		$CountdownText.text = text
-		$Tween.interpolate_property($CountdownText, "rect_scale", Vector2.ONE, Vector2.ONE*scale_mod, scale_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
-		$Tween.interpolate_property($CountdownText, "rect_position", start_pos, start_pos + $CountdownText.get_size()/2 - ($CountdownText.get_size() * scale_mod)/2, scale_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		$Tween.interpolate_property($CountdownText.get("custom_fonts/font"), "size", 30, scale_mod, scale_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT )
 		$Tween.start()
