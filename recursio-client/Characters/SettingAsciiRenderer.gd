0f6b7e8c9604587402f92104cff231cfb902e5f6
David Panagiotopulos <panagiotopulos.d@gmail.com>
2021-12-22 11:07:01 +0100

    Fix ascii settings not being updated in-game

--- recursio-client/Characters/SettingAsciiRenderer.gd
+++ recursio-client/Characters/SettingAsciiRenderer.gd
@@ -2,5 +2,14 @@ extends MeshInstance
 
 
 func _ready():
-	var x_size = UserSettings.get_setting("video", "ascii_size")
-	get_surface_material(0).set_shader_param("character_size", Vector2(x_size, x_size * 2))
+	update_ascii_size(UserSettings.get_setting("video", "ascii_size"))
+	var _error = UserSettings.connect("setting_changed", self, "_on_setting_changed")
+
+
+func _on_setting_changed(setting_header, setting_name, value):
+	if setting_header == "video" and setting_name == "ascii_size":
+		update_ascii_size(value)
+
+
+func update_ascii_size(new_x_size):
+	get_surface_material(0).set_shader_param("character_size", Vector2(new_x_size, new_x_size * 2))
