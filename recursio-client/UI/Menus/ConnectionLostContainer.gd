d4a7d6fbff949d54554ecfd7d9e2490dcc7d36a9
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-22 15:17:34 +0100

    Fix cleanup bugs

--- recursio-client/UI/Menus/ConnectionLostContainer.gd
+++ recursio-client/UI/Menus/ConnectionLostContainer.gd
@@ -1,10 +1,10 @@
  extends PanelContainer
 
 
-onready var _back_button: SoundButton = get_node("SettingsList/BackButton")
+onready var _back_button: SoundButton = get_node("ConnectionLostList/BackButton")
 
 func _ready() -> void:
-	_back_button.connect("pressed", self, "_on_back_button_pressed")
+	var _error = _back_button.connect("pressed", self, "_on_back_button_pressed")
 
 func _on_back_button_pressed() -> void:
 	hide()
