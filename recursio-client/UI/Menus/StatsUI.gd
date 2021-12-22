b892932e45b9b0fea362577788300c2479af0133
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-22 12:25:40 +0100

    Change Stats Style

--- recursio-client/UI/Menus/StatsUI.gd
+++ recursio-client/UI/Menus/StatsUI.gd
@@ -2,8 +2,15 @@ extends VBoxContainer
 class_name StatsUI
 
 
+onready var _visualisation = get_node("Values/Visualisation")
+
+func _ready():
+	ColorManager.color_object_by_property("player_main", _visualisation.get("custom_styles/fg"), "bg_color")
+	ColorManager.color_object_by_property("enemy_main", _visualisation.get("custom_styles/bg"), "bg_color")
+	
+
 func set_title(title: String) -> void:
-	$Titel.text = title
+	$Descriptions/Space/Title.text = title
 
 func set_descriptions(left_description: String, right_description: String) -> void:
 	$Descriptions/LeftDescription.text = left_description
