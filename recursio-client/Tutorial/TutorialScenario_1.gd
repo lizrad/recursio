707966a8f7d65b096bcabb2f8e80e8e3d68ad013
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-26 15:12:30 +0100

    Add peaceful enemy to tutorial one

--- recursio-client/Tutorial/TutorialScenario_1.gd
+++ recursio-client/Tutorial/TutorialScenario_1.gd
@@ -2,6 +2,10 @@ extends TutorialScenario
 class_name TutorialScenario_1
 
 
+var _player: Player
+var _enemy: Enemy
+
+
 func _ready():
 	_rounds = 2
 	_completion_delay = 2.0
@@ -14,10 +18,8 @@ func _ready():
 	add_round_end_function(funcref(self, "_completed_round_2"))
 	
 	_character_manager._on_spawn_player(0, Vector3.ZERO, 0)
-	_character_manager.get_player().kb.visible = false
+	
 	_character_manager.hide_player_button_overlay = true
-	_goal_element_1.init(_character_manager.get_player().get_camera())
-	_goal_element_2.init(_character_manager.get_player().get_camera())
 
 	# Shorten prep phase
 	_round_manager._preparation_phase_time = 3.0
@@ -26,6 +28,13 @@ func _ready():
 	_character_manager._on_spawn_enemy(1, spawn_point, 1)
 	_character_manager.enemy_is_server_driven = false
 	_character_manager.get_enemy().kb.visible = false
+	
+	_enemy = _character_manager.get_enemy()
+	_player = _character_manager.get_player()
+	
+	_player.kb.visible = false
+	_goal_element_1.init(_player.get_camera())
+	_goal_element_2.init(_player.get_camera())
 
 
 func _started_round_1():
@@ -47,16 +56,16 @@ func _started_round_1():
 	
 	_goal_element_2.hide()
 	_bottom_element.set_text("Start with this one!")
-	_character_manager.get_player().set_custom_view_target(_level.get_capture_points()[1])
+	_player.set_custom_view_target(_level.get_capture_points()[1])
 	_goal_element_1.set_text("Capture!")
 	_goal_element_1.show()
 	
 	yield(get_tree().create_timer(2), "timeout")
 	
-	_character_manager.get_player().follow_camera()
+	_player.follow_camera()
 	
 	
-	_character_manager.get_player().kb.visible = true
+	_player.kb.visible = true
 	_character_manager._round_manager._start_game()
 	yield(get_tree().create_timer(6), "timeout")
 	add_sub_condition(funcref(self, "_move_sub_condition_start"), funcref(self, "_move_sub_condition"), funcref(self, "_move_sub_condition_end"))
@@ -64,7 +73,7 @@ func _started_round_1():
 
 
 func _check_completed_round_1() -> bool:
-	return _level.get_capture_points()[1]._capture_progress >= 0.9
+	return _level.get_capture_points()[1].get_capture_progress() >= 0.9
 
 
 func _completed_round_1() -> void:
@@ -79,12 +88,27 @@ func _started_round_2() -> void:
 	
 	_character_manager._round_manager.round_index += 1
 	_character_manager._round_manager.switch_to_phase(RoundManager.Phases.PREPARATION)
+	# setting enemy timeline back to the first one
+	_character_manager._on_timeline_picked(1,0)
+	
+	_enemy.kb.visible = true
+	var _error = _enemy.connect("client_hit", self, "_on_enemy_hit")
+	_enemy.set_position(Vector3(0,0,-4))
+	var enemyAI = EnemyAI.new(_enemy)
+	enemyAI.add_waypoint(Vector2(0, -3))
+	enemyAI.set_character_to_shoot(_player)
+	enemyAI.peaceful = true
+	add_child(enemyAI)
+	enemyAI.start()
+	
+	
+	
 	print(_round_manager.round_index)
 	_bottom_element.set_text("Now get the other one.")
 	_bottom_element.set_control(TutorialUIBottomElement.Controls.None)
 	_bottom_element.show()
 	
-	_character_manager.get_player().set_custom_view_target(_level.get_capture_points()[0])
+	_player.set_custom_view_target(_level.get_capture_points()[0])
 	_goal_element_1.set_goal(_level.get_capture_points()[0])
 	_goal_element_1.set_text("Capture!")
 	_goal_element_1.show()
@@ -99,12 +123,13 @@ func _started_round_2() -> void:
 	
 	_bottom_element.set_text("Move!")
 	_bottom_element.set_control(TutorialUIBottomElement.Controls.Move)
-	_character_manager.get_player().follow_camera()
+	_player.follow_camera()
 
 
 func _check_completed_round_2() -> bool:
-	return _level.get_capture_points()[1]._capture_progress >= 0.9 \
-			and _level.get_capture_points()[0]._capture_progress >= 0.9 \
+	return _level.get_capture_points()[1].get_capture_progress() >= 0.9 \
+			and _level.get_capture_points()[0].get_capture_progress() >= 0.9 \
+			and _level.get_capture_points()[0].get_capture_team() == 0 \
 
 func _completed_round_2() -> void:
 	_level.get_capture_points()[0]._capture_progress = 1.0
@@ -118,15 +143,29 @@ func _move_sub_condition_start() -> void:
 	_bottom_element.show()
 	_bottom_element.set_text("Move!")
 	_bottom_element.set_control(TutorialUIBottomElement.Controls.Move)
+
+
 func _move_sub_condition() -> bool:
-	return (_character_manager.get_player().spawn_point - _character_manager.get_player().get_position()).length() > 5
+	return (_player.spawn_point - _player.get_position()).length() > 5
+
+
 func _move_sub_condition_end() -> void:
 	_bottom_element.hide()
+
+
 func _dash_sub_condition_start() -> void:
 	_bottom_element.show()
 	_bottom_element.set_text("Dash!")
 	_bottom_element.set_control(TutorialUIBottomElement.Controls.Dash)
+
+
 func _dash_sub_condition() -> bool:
-	return _character_manager.get_player().get_dash_ammunition() != 2
+	return _player.get_dash_ammunition() != 2
+
+
 func _dash_sub_condition_end() -> void:
 	_bottom_element.hide()
+
+
+func _on_enemy_hit(perpetrator):
+	_enemy.server_hit(perpetrator)
