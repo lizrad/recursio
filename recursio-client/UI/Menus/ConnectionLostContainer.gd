4ad00c89fcd9b37e9e861a514d802e7bfbca7b3a
David Panagiotopulos <panagiotopulos.d@gmail.com>
2021-12-28 17:13:00 +0100

    Fix GameEndScreen scaling

--- recursio-client/UI/Menus/ConnectionLostContainer.gd
+++ recursio-client/UI/Menus/ConnectionLostContainer.gd
@@ -5,6 +5,12 @@ onready var _back_button: SoundButton = get_node("ConnectionLostList/BackButton"
 
 func _ready() -> void:
 	var _error = _back_button.connect("pressed", self, "_on_back_button_pressed")
+	_error = self.connect("visibility_changed", self, "_on_visibility_changed")
+
+
+func _on_visibility_changed() -> void:
+	_back_button.grab_focus()
+
 
 func _on_back_button_pressed() -> void:
 	hide()
