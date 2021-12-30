73ce1e57939bae8860f91b2e3505a91dba35d946
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2021-12-30 15:01:34 +0100

    Use correct time for record timestamp

--- Shared/Characters/PlayerBase.gd
+++ Shared/Characters/PlayerBase.gd
@@ -31,6 +31,9 @@ var _move_acceleration = Constants.get_value("movement", "acceleration")
 # RecordManager for recording movement and actions
 var _record_manager: RecordManager = RecordManager.new()
 
+func _ready() -> void:
+	_record_manager.server = get_node("/root/Server")
+
 
 func player_base_init(action_manager) -> void:
 	.character_base_init(action_manager)
