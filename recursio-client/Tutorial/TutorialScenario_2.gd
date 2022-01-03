d6e920c666d8c43772aec9563d0e4974ff951d39
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-01-03 19:07:51 +0100

    Remove unnecessary text from tutorial 2

--- recursio-client/Tutorial/TutorialScenario_2.gd
+++ recursio-client/Tutorial/TutorialScenario_2.gd
@@ -5,8 +5,8 @@ var _player_kiled_once : bool = false
 
 func _ready():
 	# Shorten prep phase
-	_round_manager._preparation_phase_time = 0.1
-	_player._hud.add_custom_max_time("prep_phase_time", 0.1)
+	_round_manager._preparation_phase_time = 3.0
+	_player._hud.add_custom_max_time("prep_phase_time", 3.0)
 	_rounds = 3
 	add_round_start_function(funcref(self, "_started_round_1"))
 	add_round_condition_function(funcref(self, "_check_completed_round_1"))
@@ -22,34 +22,25 @@ func _ready():
 
 
 func _started_round_1():
-	add_post_process_exception(_bottom_element)
-	_bottom_element.show()
-	_bottom_element.set_content("Welcome to the second tutorial!", TutorialUIBottomElement.Controls.None, true)
-	
 	var _error = _player.connect("client_hit", self, "_on_player_hit")
 	_error = _enemy.connect("client_hit", self, "_on_enemy_hit")
-	
 	_enemyAI = EnemyAI.new(_enemy)
 	_enemyAI.add_waypoint(Vector2(-5, 5))
-	
 	_enemyAI.set_character_to_shoot(_player)
-	
 	add_child(_enemyAI)
-	_enemyAI.start()
-	pause()
-	yield(_bottom_element, "continue_pressed")
-	unpause()
 	
-	_character_manager._round_manager._start_game()
-	_bottom_element.set_content("")
-	
+	_goal_element_1.set_content("Capture!", _level.get_capture_points()[1])
+	_goal_element_1.show()
+	_player.set_custom_view_target(_level.get_capture_points()[1])
 	_player.kb.visible = true
 	_enemy.kb.visible = true
-	
+	_character_manager._round_manager._start_game()
 	yield(_round_manager, "game_phase_started")
-	
 	_enemy.set_position(Vector3(-12,0, 8))
+	_enemyAI.start()
 	_player.toggle_movement(false)
+	
+	_bottom_element.show()
 	_bottom_element.set_content("Wait 3s")
 	yield(get_tree().create_timer(1), "timeout")
 	_bottom_element.set_content("Wait 2s")
@@ -58,40 +49,24 @@ func _started_round_1():
 	yield(get_tree().create_timer(1), "timeout")
 	_bottom_element.set_content("Wait 0s")
 
+	_bottom_element.hide()
 	_player.toggle_trigger(ActionManager.Trigger.SPECIAL_MOVEMENT_START, true)
 	_player.toggle_movement(true)
 	
-	_bottom_element.set_content("Capture this point!")
-	_goal_element_1.show()
-	_goal_element_1.set_content("Capture!", _level.get_capture_points()[1])
 	
 	
 	
 	# Wait until player gets hit
 	yield(_player, "client_hit")
-	_bottom_element.set_content("You got hit!", TutorialUIBottomElement.Controls.None, true)
 	_player_kiled_once = true
-	pause()
-	yield(_bottom_element, "continue_pressed")
-	unpause()
-	
-	add_post_process_exception(_goal_element_1)
-	add_post_process_exception(_enemy)
-	_player.set_custom_view_target(_enemy.get_body())
 	_goal_element_1.set_content("Kill!", _enemy.get_body())
-	_bottom_element.set_content("Kill the enemy before they can kill you!", TutorialUIBottomElement.Controls.None, true)
-	
-	pause()
-	yield(_bottom_element, "continue_pressed")
-	unpause()
-	remove_post_process_exception(_enemy)
-	_player.follow_camera()
 	_player.toggle_trigger(ActionManager.Trigger.DEFAULT_ATTACK_START, true)
 	_player.toggle_trigger(ActionManager.Trigger.FIRE_START, true)
+	_bottom_element.show()
 	_bottom_element.set_content("Shoot!", TutorialUIBottomElement.Controls.Shoot)
 	
 	yield(_enemy, "client_hit")
-	_bottom_element.set_content("Now capture the point.")
+	_bottom_element.hide()
 	_goal_element_1.set_content("Capture!", _level.get_capture_points()[1])
 
 
@@ -105,12 +80,6 @@ func _completed_round_1() -> void:
 
 func _started_round_2() -> void:
 	_goal_element_1.hide()
-	
-	_bottom_element.set_content("Good job!", TutorialUIBottomElement.Controls.None, true)
-	pause()
-	yield(_bottom_element, "continue_pressed")
-	unpause()
-	
 	_character_manager._round_manager.round_index += 1
 	_character_manager._round_manager.switch_to_phase(RoundManager.Phases.PREPARATION)
 	_enemy.kb.visible = false
@@ -119,8 +88,9 @@ func _started_round_2() -> void:
 	for ghost in _ghost_manager._ghosts:
 		var _error = ghost.connect("client_hit", self, "_on_ghost_hit", [ghost])
 	
+	_bottom_element.show()
 	_bottom_element.set_content("Now watch what happens with your ghost.")
-	
+	_character_manager.toggle_player_input_pause(true)
 	yield(_round_manager, "game_phase_started")
 	_player.move_camera_to_overview()
 	_goal_element_1.show()
@@ -136,6 +106,8 @@ func _check_completed_round_2() -> bool:
 
 func _completed_round_2() -> void:
 	add_post_process_exception(_ghost_manager._player_ghosts[0].get_body())
+	add_post_process_exception(_bottom_element)
+	add_post_process_exception(_goal_element_1)
 	_bottom_element.set_content("Killing your past timeline stops it completely.", TutorialUIBottomElement.Controls.None, true)
 	
 var _soft_lock = false
@@ -144,17 +116,24 @@ func _started_round_3() -> void:
 	yield(_bottom_element, "continue_pressed")
 	unpause()
 	remove_post_process_exception(_ghost_manager._player_ghosts[0].get_body())
+	remove_post_process_exception(_bottom_element)
+	remove_post_process_exception(_goal_element_1)
 	_ghost_manager._player_ghosts[0].disconnect("client_hit", self, "_on_ghost_hit")
 	var _error = _ghost_manager._player_ghosts[0].connect("client_hit", self, "_on_ghost_hit_soft_lock", [_ghost_manager._player_ghosts[0]])
 	
 	
 	_goal_element_1.hide()
-	_bottom_element.set_content("Prevent your past death!")
+	_bottom_element.hide()
+	_character_manager.toggle_player_input_pause(false)
+	_player.toggle_trigger(ActionManager.Trigger.FIRE_START, true)
+	_player.toggle_trigger(ActionManager.Trigger.DEFAULT_ATTACK_START, true)
+	_player.toggle_trigger(ActionManager.Trigger.SPECIAL_MOVEMENT_START, true)
 	_character_manager._round_manager.switch_to_phase(RoundManager.Phases.PREPARATION)
 	_player.kb.visible = true
 	_enemy.kb.visible = true
 	yield(_round_manager, "game_phase_started")
 	
+	_bottom_element.show()
 	_bottom_element.set_content("Spawn Wall!", TutorialUIBottomElement.Controls.Shoot)
 	_goal_element_1.show()
 	_goal_element_1.set_content("Place here",_ghost_manager._enemy_ghosts[0].get_body())
@@ -164,33 +143,31 @@ func _started_round_3() -> void:
 	_goal_element_2.set_content("Repeats", _ghost_manager._player_ghosts[0].get_body())
 	
 	yield(_ghost_manager._player_ghosts[0], "respawned")
-	print("here")
 	# wait if we really aren't soft locked
 	yield(get_tree(),"idle_frame")
-	print(_soft_lock)
 	if not _soft_lock:
-		_start_round_3_end()
+		_round_3_end_sequence()
 	
 
-func _start_round_3_end():
+func _round_3_end_sequence():
 	_player.set_custom_view_target(_ghost_manager._player_ghosts[0].get_body())
-	add_post_process_exception(_goal_element_2)
+	add_post_process_exception(_bottom_element)
+	add_post_process_exception(_goal_element_1)
 	add_post_process_exception(_ghost_manager._player_ghosts[0])
-	_goal_element_2.set_content("Respawned", _ghost_manager._player_ghosts[0].get_body())
+	_goal_element_1.set_content("Respawned", _ghost_manager._player_ghosts[0].get_body())
+	_bottom_element.show()
 	_bottom_element.set_content("If the ghost does not get hit, it just gets set back to spawn.", TutorialUIBottomElement.Controls.None, true)
 	pause()
-	_goal_element_1.hide()
 	yield(_bottom_element, "continue_pressed")
 	unpause()
-	remove_post_process_exception(_goal_element_2)
+	_bottom_element.hide()
+	remove_post_process_exception(_goal_element_1)
 	remove_post_process_exception(_ghost_manager._player_ghosts[0])
-	_goal_element_2.set_content("Repeats further", _ghost_manager._player_ghosts[0].get_body())
-	_bottom_element.set_content("Now the ghost can capture the point again.")
+	_goal_element_1.set_content("Repeats further", _ghost_manager._player_ghosts[0].get_body())
 	_player.follow_camera()
 	yield(_level.get_capture_points()[1], "captured")
-	_goal_element_1.show()
-	_bottom_element.set_content("You have to get the other point!")
-	_goal_element_1.set_content("Capture",_level.get_capture_points()[0])
+	_goal_element_2.show()
+	_goal_element_2.set_content("Capture",_level.get_capture_points()[0])
 
 func _check_completed_round_3() -> bool:
 	return _level.get_capture_points()[1].capture_progress == 1.0 \
@@ -198,7 +175,6 @@ func _check_completed_round_3() -> bool:
 
 
 func _completed_round_3() -> void:
-	remove_post_process_exception(_goal_element_1)
 	pass
 
 
@@ -217,11 +193,12 @@ func _on_ghost_hit(perpetrator, ghost):
 
 
 func _on_ghost_hit_soft_lock(perpetrator, ghost: PlayerGhost):	
-	print("_on_ghost_hit_soft_lock")
+	print("a;skldfja;lsdjf;")
 	_soft_lock = true
 	ghost.toggle_visibility_light(false)
 	ghost.server_hit(perpetrator)
-	
+	_bottom_element.show()
+	add_post_process_exception(_bottom_element)
 	if perpetrator is Player:
 		_bottom_element.set_content("Oh no, you killed your ghost! Try using a melee attack.", TutorialUIBottomElement.Controls.None, true)
 	else:
@@ -231,6 +208,7 @@ func _on_ghost_hit_soft_lock(perpetrator, ghost: PlayerGhost):
 	pause()
 	yield(_bottom_element, "continue_pressed")
 	unpause()
+	remove_post_process_exception(_bottom_element)
 	_character_manager._round_manager.switch_to_phase(RoundManager.Phases.PREPARATION)
 	_goal_element_1.show()
 	_goal_element_2.show()
@@ -244,6 +222,7 @@ func _on_ghost_hit_soft_lock(perpetrator, ghost: PlayerGhost):
 		
 	
 	yield(_ghost_manager._player_ghosts[0], "respawned")
+	# wait if we really aren't soft locked
 	yield(get_tree(), "idle_frame")
 	if  _ghost_manager._player_ghosts[0]._is_playing and not _ghost_manager._player_ghosts[0].currently_dying and not _ghost_manager._player_ghosts[0].currently_spawning: 
-		_start_round_3_end()
+		_round_3_end_sequence()
