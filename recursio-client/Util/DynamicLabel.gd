b7714942cb9c5c3ccf7a722ba78bc729be050dfe
David Panagiotopulos <panagiotopulos.d@gmail.com>
2022-01-03 17:24:17 +0100

    Add simple scaling label

---
+++ recursio-client/Util/DynamicLabel.gd
@@ -0,0 +1,29 @@
+tool
+extends Label
+class_name DynamicLabel
+
+export(bool) var enable_this_after_font_is_unique: bool = false
+export(int) var font_size_min: int = 2
+export(int) var font_size_max: int = 72
+# The font gives chars a different width, 
+# therefore setting the pixel size of the font does not match the actual size
+export(float) var font_char_scale: float = 1.4
+
+onready var _last_size: Vector2 = self.rect_size
+
+
+func _process(_delta):
+	if not enable_this_after_font_is_unique:
+		return
+	
+	if Engine.editor_hint:
+		_update_font_size()
+	
+	if _last_size != self.rect_size:
+		_update_font_size()
+
+
+func _update_font_size():
+	var font = self.get("custom_fonts/font")
+	var new_font_size = (self.rect_size.x / self.text.length()) * font_char_scale
+	font.size = min(max(new_font_size, font_size_min), font_size_max)
