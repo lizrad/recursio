476fe71a48698f1d67998cc7ef08dded2d2b2eb6
karl <karl.bittner@hexaquo.at>
2022-01-08 00:39:40 +0100

    Hotfix for hotfix for #174
    our hotfix broke the tutorial, so we keep the partially broken code for that

--- recursio-client/Managers/ClientGhostManager.gd
+++ recursio-client/Managers/ClientGhostManager.gd
@@ -73,19 +73,20 @@ func _enable_active_ghosts() -> void:
 
 # OVERRIDE ABSTRACT #
 func _use_new_record_data():
-	# FIXME: This seems to cause wrong ghosts and paths to show up. It's not strictly necessary,
-	# just makes things show up faster on the client
-	pass
-#	var current_round_index = _round_manager.round_index-1
-#	var record_data = _character_manager._player.get_record_data()
-#	if current_round_index > _player_ghosts[record_data.timeline_index].round_index:
-#		_update_ghost_record(_player_ghosts, record_data.timeline_index, record_data, current_round_index)
-#		_player_ghosts[record_data.timeline_index].player_id = _character_manager._player.player_id
-#
-#	record_data = _character_manager._enemy.get_record_data()
-#	if current_round_index > _enemy_ghosts[record_data.timeline_index].round_index:
-#		_update_ghost_record(_enemy_ghosts, record_data.timeline_index, record_data, current_round_index)
-#		_enemy_ghosts[record_data.timeline_index].player_id = _character_manager._enemy.player_id
+	# FIXME: This seems to cause wrong ghosts and paths to show up. It's not strictly necessary
+	# (except for the tutorial, which is the reason for this check), it just makes things show up
+	# faster on the client
+	if not Server.is_connection_active:
+		var current_round_index = _round_manager.round_index-1
+		var record_data = _character_manager._player.get_record_data()
+		if current_round_index > _player_ghosts[record_data.timeline_index].round_index:
+			_update_ghost_record(_player_ghosts, record_data.timeline_index, record_data, current_round_index)
+			_player_ghosts[record_data.timeline_index].player_id = _character_manager._player.player_id
+
+		record_data = _character_manager._enemy.get_record_data()
+		if current_round_index > _enemy_ghosts[record_data.timeline_index].round_index:
+			_update_ghost_record(_enemy_ghosts, record_data.timeline_index, record_data, current_round_index)
+			_enemy_ghosts[record_data.timeline_index].player_id = _character_manager._enemy.player_id
 
 
 # OVERRIDE ABSTRACT #
