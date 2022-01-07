f7db2b5e1479fe7b172a203f7fd920f3f03da896
Karl <karl.bittner@hexaquo.at>
2022-01-07 14:09:10 +0100

    Always play in-game music
    I misjudged the round length, we'll have to make it dynamic

--- recursio-client/Managers/GameMusicHandler.gd
+++ recursio-client/Managers/GameMusicHandler.gd
@@ -6,9 +6,10 @@ onready var round_manager: RoundManager = get_node(round_manager_path)
 
 
 func _ready():
-	var error = round_manager.connect("game_phase_started", self, "play_game_music")
+	var error = round_manager.connect("preparation_phase_started", self, "play_game_music")
 	assert(error == OK)
 
 
 func play_game_music():
-	$AudioStreamPlayer.play()
+	if not $AudioStreamPlayer.playing:
+		$AudioStreamPlayer.play()
