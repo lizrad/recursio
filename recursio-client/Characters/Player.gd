2157f976e2e4c3bde4d91918f3bd409f2871d1a1
incredibleLeitman <michael.leithner@gmail.com>
2022-01-13 23:06:36 +0100

    fix symptom if player tries to shoot in prep_phase

--- recursio-client/Characters/Player.gd
+++ recursio-client/Characters/Player.gd
@@ -135,8 +135,10 @@ func apply_input(movement_vector: Vector3, rotation_vector: Vector3, buttons: in
 		var action = _get_action(ActionManager.Trigger.FIRE_START, timeline_index) as Action
 		if action.ammunition > 0:
 			_aim_visuals.visible = true
-			_aim_visuals.get_child(timeline_index % 2).visible = aim_mode
-			PostProcess.chromatic_ab_strength = 0.1
+			# crashes in tutorial if no timeline is set yet and player tries to shoot
+			if timeline_index > 0:
+				_aim_visuals.get_child(timeline_index % 2).visible = aim_mode
+				PostProcess.chromatic_ab_strength = 0.1
 		else:
 			# -> already handles sound effects but fail sound for no ammo is only needed for current player
 			if not _audio_player.playing:
